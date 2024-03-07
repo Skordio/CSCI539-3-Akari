480620cdbc51312bb67fa8fbf8a56d4582ebd0f1
Skordio <skordion@hotmail.com>
2024-03-07 12:56:29 -0500

    first attempt at adjusting file writing process to make akari files instead of maze files

--- akari.py
+++ akari.py
@@ -74,19 +74,6 @@ class Akari:
             self.set_grid_size(grid_size_x, grid_size_y)
             self.reset_cells()
             
-            start_cell_x_byte = maze_file.read(1)
-            start_cell_y_byte = maze_file.read(1)
-            
-            start_cell_x = int.from_bytes(start_cell_x_byte, "big")
-            start_cell_y = int.from_bytes(start_cell_y_byte, "big")
-            
-            
-            end_cell_x_byte = maze_file.read(1)
-            end_cell_y_byte = maze_file.read(1)
-            
-            end_cell_x = int.from_bytes(end_cell_x_byte, "big")
-            end_cell_y = int.from_bytes(end_cell_y_byte, "big")
-            
             x = 0
             y = 0
             while (byte := maze_file.read(1)) and y < grid_size_y:
@@ -95,16 +82,16 @@ class Akari:
                 
                 firstFour = byte_str[:4]
                 
-                # self.cells[(x, y)].walls['top'] = firstFour[0] == '1'
-                # self.cells[(x, y)].walls['right'] = firstFour[1] == '1'
-                # self.cells[(x, y)].walls['bottom'] = firstFour[2] == '1'
-                # self.cells[(x, y)].walls['left'] = firstFour[3] == '1'
-                
-                lastFour = byte_str[4:]
-                cell_number = int(lastFour, base=2)
-                
-                self.cells[(x, y)].number = cell_number if cell_number != 0 else None
-                
+                if firstFour == '0000':
+                    self.cells[(x, y)].is_black = False
+                else:
+                    self.cells[(x, y)].is_black = True
+                    
+                    lastFour = byte_str[4:]
+                    cell_number = int(lastFour, base=2)
+                    
+                    self.cells[(x, y)].number = cell_number if cell_number != 0 else None
+                    
                 x += 1
                 if x == grid_size_x:
                     x = 0
@@ -113,46 +100,35 @@ class Akari:
 
     # TODO: Fix this method for Akari instead of Maze
     def save_to_file(self, filename):
-        with open(os.path.join('mazes', filename), 'wb') as maze_file:
-            maze_file.write(int(self.grid_size_x).to_bytes(1, 'big'))
-            maze_file.write(int(self.grid_size_y).to_bytes(1, 'big'))
+        with open(os.path.join('mazes', filename), 'wb') as akari_file:
+            akari_file.write(int(self.grid_size_x).to_bytes(1, 'big'))
+            akari_file.write(int(self.grid_size_y).to_bytes(1, 'big'))
                     
-            start_cell = next((cell for cell in self.cells.values() if cell.is_start), None)
-            end_cell = next((cell for cell in self.cells.values() if cell.is_end), None)
+            # start_cell = next((cell for cell in self.cells.values() if cell.is_start), None)
+            # end_cell = next((cell for cell in self.cells.values() if cell.is_end), None)
             
-            maze_file.write(int(start_cell.x).to_bytes(1, 'big'))
-            maze_file.write(int(start_cell.y).to_bytes(1, 'big'))
+            # akari_file.write(int(start_cell.x).to_bytes(1, 'big'))
+            # akari_file.write(int(start_cell.y).to_bytes(1, 'big'))
             
-            maze_file.write(int(end_cell.x).to_bytes(1, 'big'))
-            maze_file.write(int(end_cell.y).to_bytes(1, 'big'))
+            # akari_file.write(int(end_cell.x).to_bytes(1, 'big'))
+            # akari_file.write(int(end_cell.y).to_bytes(1, 'big'))
             
             for y in range(self.grid_size_y):
                 for x in range(self.grid_size_x):
                     cell = self.cells[(x, y)]
                     byte = ''
                     
-                    if cell.walls['top']:
-                        byte += '1'
+                    if not cell.is_black:
+                        byte = '00000000'
                     else:
-                        byte += '0'
-                    if cell.walls['right']:
-                        byte += '1'
-                    else:
-                        byte += '0'
-                    if cell.walls['bottom']:
-                        byte += '1'
-                    else:
-                        byte += '0'
-                    if cell.walls['left']:
-                        byte += '1'
-                    else:
-                        byte += '0'
+                        byte = '1000'
                     
-                    if cell.number is not None:
-                        byte += bin(cell.number)[2:].rjust(4, '0')
-                    else:
-                        byte += '0000'
+                        if cell.number is not None:
+                            byte += bin(cell.number)[2:].rjust(4, '0')
+                        else:
+                            byte += '0000'
+                        
                     
-                    maze_file.write(int(byte, base=2).to_bytes(1, 'big'))
+                    akari_file.write(int(byte, base=2).to_bytes(1, 'big'))
 
             
